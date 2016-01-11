8a3fb54b3c5d1f6efc781b7d090e8bf9d9c54625
Paul Betts <paul@paulbetts.org>
2016-01-10 22:36:48 -0800

    Initial Import

---
+++ index.js
@@ -0,0 +1,19 @@
+require('babel-register')({
+  ignore: false,
+  presets: ['es2015', 'stage-0']
+});
+
+var pkg = require('./package.json');
+
+var exports = {};
+var deps = Object.keys(pkg.dependencies);
+
+for (var i=0; i < deps.length; i++) {
+  var cur = deps[i];
+  if (!cur.match(/^build-/i)) continue;
+
+  console.log("Loading " + cur);
+  exports[cur] = require(cur).provideBuilder();
+}
+
+module.exports = exports;
